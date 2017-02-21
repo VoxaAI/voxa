26b8ccfe09bff23d7c477884d5e7f5dee7149734
Andres Reyes Monge <armonge@gmail.com>
2017-02-21 09:59:40 -0600

    Adding some tests

--- test/plugins/reprompt-on-bad-response.spec.js
+++ test/plugins/reprompt-on-bad-response.spec.js
@@ -43,13 +43,87 @@ describe('BadResponseRepromptPlugin', () => {
       },
     };
 
+    badResponseReprompt(stateMachineSkill);
+    return stateMachineSkill.execute(event)
+      .then((reply) => {
+        expect(reply.response.outputSpeech.ssml).to.equal('<speak>Say stop if you want to finish the playback</speak>');
+      });
+  });
+
+  it('should prepend the startView', () => {
+    const stateMachineSkill = new StateMachineSkill('appId', { Model, variables, views });
+    stateMachineSkill.onIntent('entry', { });
+    stateMachineSkill.onState('playing', (request) => {
+      if (request.intent.name === 'AMAZON.StopIntent') {
+        return { reply: 'ExitIntent.Farewell' };
+      }
+
+      return null;
+    });
+
+    const event = {
+      request: {
+        type: 'IntentRequest',
+        intent: {
+          name: 'AMAZON.YesIntent',
+        },
+      },
+      session: {
+        new: false,
+        application: {
+          applicationId: 'appId',
+        },
+        attributes: {
+          state: 'playing',
+          reprompt: 'Playing.SayStop',
+        },
+      },
+    };
+
     badResponseReprompt(stateMachineSkill, {
       startView: 'BadInput.RepeatLastAskReprompt',
-      views,
     });
     return stateMachineSkill.execute(event)
       .then((reply) => {
         expect(reply.response.outputSpeech.ssml).to.equal('<speak>I\'m sorry. I didn\'t understand.\nSay stop if you want to finish the playback</speak>');
+      });
+  });
+
+  it('should just exit if no reprompt', () => {
+    const stateMachineSkill = new StateMachineSkill('appId', { Model, variables, views });
+    stateMachineSkill.onIntent('entry', { });
+    stateMachineSkill.onState('playing', (request) => {
+      if (request.intent.name === 'AMAZON.StopIntent') {
+        return { reply: 'ExitIntent.Farewell' };
+      }
+
+      return null;
+    });
+
+    const event = {
+      request: {
+        type: 'IntentRequest',
+        intent: {
+          name: 'AMAZON.YesIntent',
+        },
+      },
+      session: {
+        new: false,
+        application: {
+          applicationId: 'appId',
+        },
+        attributes: {
+          state: 'playing',
+        },
+      },
+    };
+
+    badResponseReprompt(stateMachineSkill, {
+      startView: 'BadInput.RepeatLastAskReprompt',
+    });
+    return stateMachineSkill.execute(event)
+      .then((reply) => {
+        expect(reply.response.outputSpeech.ssml).to.equal('<speak>An unrecoverable error occurred.</speak>');
       });
   });
 });
