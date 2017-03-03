d288652e856df9c2998eba7f0ebf844e2070d061
Andres Reyes Monge <armonge@gmail.com>
2017-03-03 10:56:08 -0600

    Working on the development server and documentation

--- run-ci.sh
+++ run-ci.sh
@@ -4,6 +4,21 @@ set -ev
 npm run test-ci
 npm run cobertura
 npm run lint
+npm link
+
+PROJECT_DIRECTORY=$(pwd)
+
+for SAMPLE_DIRECTORY in `find ./samples -maxdepth 1 -mindepth 1 -type d` ; do
+  cd "$PROJECT_DIRECTORY/$SAMPLE_DIRECTORY"
+  for file in config/*.json; do
+    python2 -m json.tool $file
+  done
+  npm link voxa
+  npm install 
+  cp config/local.json.example config/local.json
+  npm run lint
+  npm run test
+done
 
 if [ "${CI}" = "true" ]; then
   npm install coveralls
