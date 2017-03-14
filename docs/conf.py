8c48a69e7d7a07805dc959dba91b3017eb7c330b
Andrés Reyes Monge <armonge@gmail.com>
2017-03-13 22:53:16 -0600

    More on readthedocs
    https://docs.readthedocs.io/en/latest/vcs.html

--- docs/conf.py
+++ docs/conf.py
@@ -90,6 +90,14 @@ html_theme = 'default'
 #
 # html_theme_options = {}
 
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "mediarain", # Username
+    "github_repo": "voxa", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
