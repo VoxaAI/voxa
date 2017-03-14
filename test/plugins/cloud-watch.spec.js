6a45cece5d8e135f121c621060107370fe9efb06
Carlos A. Vaca Morales <carlosv@rain.agency>
2017-03-14 09:01:28 -0600

    Send metric data to cloud-watch plugin every time that onBeforeReplySent is executed

--- test/plugins/cloud-watch.spec.js
+++ test/plugins/cloud-watch.spec.js
@@ -14,6 +14,47 @@ const views = require('../views');
 const variables = require('../variables');
 
 describe('CloudwatchPlugin', () => {
+
+  it('should use plugin to log every time that onBeforeReplySent function is executed', () => {
+    const stateMachineSkill = new StateMachineSkill({  views, variables });
+    const cloudwatch = { putMetricData: (data, callback) => callback(null, 'foobar') };
+    const cloudwatchMock = simple.mock(cloudwatch, 'putMetricData');
+    const eventMetric = {
+      Namespace: 'fooBarSkill'
+    };
+
+    const event = {
+      request: {
+        type: 'IntentRequest',
+        intent: {
+          name: 'SomeIntent',
+        },
+      },
+      session: {
+        new: false,
+        application: {
+          applicationId: 'appId',
+        },
+      },
+    };
+
+    cloudwatchPlugin(stateMachineSkill, cloudwatch, eventMetric);
+
+    stateMachineSkill.onIntent('SomeIntent', () => 'fooStatement');
+
+    return stateMachineSkill.execute(event)
+      .then((reply) => {
+
+        expect(reply.msg.statements[0]).not.equal('An unrecoverable error occurred.');
+        expect(cloudwatchMock.called).to.be.true;
+        expect(cloudwatchMock.callCount).to.be.at.most(1);
+        expect(cloudwatchMock.lastCall.args[0].Namespace).to.equal('fooBarSkill');
+        expect(cloudwatchMock.lastCall.args[0].MetricData[0].MetricName).to.equal('Caught Error');
+        expect(cloudwatchMock.lastCall.args[0].MetricData[0].Unit).to.equal('Count');
+        expect(cloudwatchMock.lastCall.args[0].MetricData[0].Value).to.equal(1);
+      });
+  });
+
   it('should use plugin to log error onError Flow', () => {
     const stateMachineSkill = new StateMachineSkill({ variables, views });
     const spySkill = simple.stub().throwWith(new Error('foo random error'));
@@ -88,4 +129,5 @@ describe('CloudwatchPlugin', () => {
         expect(cloudwatchMock.lastCall.args[0].MetricData[0].Value).to.equal(1);
       });
   });
+
 });
