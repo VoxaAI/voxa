7abeba6d1c465870d5b355886cd42d58ca7dcc39
Andrés Reyes Monge <armonge@gmail.com>
2018-01-08 16:36:42 -0600

    Fixing index references

--- index.js
+++ index.js
@@ -8,32 +8,32 @@
 'use strict';
 
 const packageInfo = require('./package.json');
-const VoxaApp = require('./lib/VoxaApp').VoxaApp;
+const VoxaApp = require('./lib/src/VoxaApp').VoxaApp;
 
 /**
  * Plugins
  */
-const replaceIntent = require('./lib/plugins/replace-intent');
-const stateFlow = require('./lib/plugins/state-flow');
-const autoLoad = require('./lib/plugins/auto-load');
+const replaceIntent = require('./lib/src/plugins/replace-intent');
+const stateFlow = require('./lib/src/plugins/state-flow');
+const autoLoad = require('./lib/src/plugins/auto-load');
 
-const CortanaAdapter = require('./lib/adapters/cortana/CortanaAdapter').CortanaAdapter;
+const CortanaAdapter = require('./lib/src/adapters/cortana/CortanaAdapter').CortanaAdapter;
 
-const AlexaDirectives = require('./lib/adapters/alexa/directives');
-const DialogFlowDirectives = require('./lib/adapters/dialog-flow/directives');
-const CortanaDirectives = require('./lib/adapters/cortana/directives');
+const AlexaDirectives = require('./lib/src/adapters/alexa/directives');
+const DialogFlowDirectives = require('./lib/src/adapters/dialog-flow/directives');
+const CortanaDirectives = require('./lib/src/adapters/cortana/directives');
 
 
 module.exports = VoxaApp;
 
-module.exports.Alexa = require('./lib/adapters/alexa/AlexaAdapter').AlexaAdapter;
-module.exports.Alexa.DisplayTemplate = require('./lib/adapters/alexa/DisplayTemplateBuilder').DisplayTemplate;
+module.exports.Alexa = require('./lib/src/adapters/alexa/AlexaAdapter').AlexaAdapter;
+module.exports.Alexa.DisplayTemplate = require('./lib/src/adapters/alexa/DisplayTemplateBuilder').DisplayTemplate;
 module.exports.Alexa.RenderTemplate = AlexaDirectives.RenderTemplate;
 module.exports.Alexa.AccountLinkingCard = AlexaDirectives.AccountLinkingCard;
 module.exports.Alexa.PlayAudio = AlexaDirectives.PlayAudio;
 module.exports.Alexa.DialogDelegate = AlexaDirectives.DialogDelegate;
 
-module.exports.DialogFlow = require('./lib/adapters/dialog-flow/DialogFlowAdapter').DialogFlowAdapter;
+module.exports.DialogFlow = require('./lib/src/adapters/dialog-flow/DialogFlowAdapter').DialogFlowAdapter;
 module.exports.DialogFlow.List = DialogFlowDirectives.List;
 module.exports.DialogFlow.Suggestions = DialogFlowDirectives.Suggestions;
 module.exports.DialogFlow.BasicCard = DialogFlowDirectives.BasicCard;
@@ -42,7 +42,7 @@ module.exports.Cortana = CortanaAdapter;
 module.exports.Cortana.HeroCard = CortanaDirectives.HeroCard;
 module.exports.Cortana.SuggestedActions = CortanaDirectives.SuggestedActions;
 
-module.exports.Renderer = require('./lib/renderers/Renderer').Renderer
+module.exports.Renderer = require('./lib/src/renderers/Renderer').Renderer
 
 module.exports.version = packageInfo.version;
 
