c8632787c99a8446fd4d893ba485ebe0488ef6b6
Andrés Reyes Monge <armonge@gmail.com>
2018-07-03 09:05:00 -0600

    Making it work locally when no TRAVIS_NODE_VERSION is set

--- run-ci.sh
+++ run-ci.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-set -ev
+set -euo pipefail
+set -o xtrace
 
 yarn run test-ci
 yarn run report
 yarn run lint
 
 npx typedoc --out typedoc --name Voxa --readme ./README.md --target ES5 ./src
-
-docker pull "lambci/lambda:nodejs$TRAVIS_NODE_VERSION"
+NODE_VERSION=${TRAVIS_NODE_VERSION:-}
+if [ ! -z ${NODE_VERSION} ];
+then
+  docker pull "lambci/lambda:nodejs$TRAVIS_NODE_VERSION"
+else
+  NODE_VERSION=$(node --version | cut -d 'v' -f2 | cut -d'.' -f1,2)
+  docker pull "lambci/lambda:nodejs$NODE_VERSION"
+fi
 (
   cd hello-world
   yarn
