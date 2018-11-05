52b2788908bc2ed64245c3264209288ddad0bfa1
Andrés Reyes Monge <armonge@gmail.com>
2018-11-05 11:47:09 -0800

    Fixes a bug that prevented the SM from using the global intent handlers

---
+++ test/States.spec.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2018 Rain Agency <contact@rain.agency>
+ * Author: Rain Agency <contact@rain.agency>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { expect } from "chai";
+import * as _ from "lodash";
+import { AlexaPlatform, IVoxaEvent, VoxaApp } from "../src";
+import { AlexaRequestBuilder } from "./tools";
+import { variables } from "./variables";
+import { views } from "./views";
+
+describe("States", () => {
+  let voxaApp: VoxaApp;
+  let alexaSkill: AlexaPlatform;
+  let rb: AlexaRequestBuilder;
+
+  beforeEach(() => {
+    rb = new AlexaRequestBuilder();
+    voxaApp = new VoxaApp({ variables, views });
+
+    voxaApp.onIntent("HelpIntent", {
+      flow: "yield",
+      sayp: "Help",
+      to: "entry",
+    });
+
+    voxaApp.onState(
+      "helpSettings",
+      {
+        flow: "yield",
+        sayp: "question",
+        to: "entry",
+      },
+      "YesIntent",
+    );
+
+    voxaApp.onState(
+      "helpSettings",
+      (voxaEvent: IVoxaEvent) => {
+        return {
+          flow: "yield",
+          sayp: "question",
+          to: "entry",
+        };
+      },
+      "NoIntent",
+    );
+
+    alexaSkill = new AlexaPlatform(voxaApp);
+  });
+
+  it("should correctly transition to the global handler after failling to find a correct handler", async () => {
+    const helpRequest = rb.getIntentRequest("AMAZON.HelpIntent");
+    _.set(helpRequest, "session.new", false);
+    _.set(helpRequest, "session.attributes", {
+      model: {},
+      state: "helpSettings",
+    });
+    const reply = await alexaSkill.execute(helpRequest);
+    expect(reply.response.shouldEndSession).to.be.false;
+    expect(reply.sessionAttributes).to.deep.equal({
+      model: {},
+      state: "entry",
+    });
+    expect(reply.response.outputSpeech.ssml).to.equal("<speak>Help</speak>");
+  });
+});
