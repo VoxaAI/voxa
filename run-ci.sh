ec9df194882b25bb3f807e3568101233f6e63994
Andrés Reyes Monge <armonge@gmail.com>
2019-02-13 09:47:03 -0800

    Updated run-ci.sh and hello-world.spec

--- run-ci.sh
+++ run-ci.sh
@@ -7,13 +7,13 @@ yarn run report
 yarn run lint
 
 npx typedoc --out typedoc --name Voxa --readme ./README.md --target ES5 --ignoreCompilerErrors ./src
-NODE_VERSION=${TRAVIS_NODE_VERSION:-}
-if [ ! -z "${NODE_VERSION}" ]; then
-  docker pull "lambci/lambda:nodejs$TRAVIS_NODE_VERSION"
-else
-  NODE_VERSION=$(node --version | cut -d 'v' -f2 | cut -d'.' -f1,2)
-  docker pull "lambci/lambda:nodejs$NODE_VERSION"
+export NODE_VERSION=${TRAVIS_NODE_VERSION:-}
+if [ -z "${NODE_VERSION}" ]; then
+  export NODE_VERSION=$(node --version | cut -d 'v' -f2 | cut -d'.' -f1,2)
 fi
+
+docker pull "lambci/lambda:nodejs$NODE_VERSION"
+
 (
   cd hello-world
   rm -rf node_modules
@@ -21,6 +21,6 @@ fi
   yarn mocha hello-world.spec.js
 )
 
-if [ "${CI}" = "true" ]; then
+if [ "${CI:-}" = "true" ]; then
   cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
 fi
