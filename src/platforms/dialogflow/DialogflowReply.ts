ec97a6e925d8fbb424c40551cc57c94f0ed9cf41
omenocal <octavio_menocal92@hotmail.es>
2019-02-14 18:56:37 -0600

    Added source key back to DialogflowReply

--- src/platforms/dialogflow/DialogflowReply.ts
+++ src/platforms/dialogflow/DialogflowReply.ts
@@ -54,6 +54,7 @@ export interface IGooglePayload extends IDialogflowPayload {
 export class DialogflowReply implements IVoxaReply {
   public outputContexts: GoogleCloudDialogflowV2Context[] = [];
   public fulfillmentText: string = "";
+  public source: string = "google";
   public payload: IDialogflowPayload;
 
   constructor() {
